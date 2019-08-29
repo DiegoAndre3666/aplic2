41b259dc1dcb441192d6f99b8d1d0af6b72d3b93
Diego Andre <diegotadsfaccar@gmail.com>
2019-08-29 20:49:53 -0300

    atualizacao banco de dados

--- inventario.sql
+++ inventario.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: 29-Ago-2019 às 00:39
+-- Generation Time: 30-Ago-2019 às 01:46
 -- Versão do servidor: 10.1.38-MariaDB
 -- versão do PHP: 7.3.2
 
@@ -25,6 +25,17 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Estrutura da tabela `dpto`
+--
+
+CREATE TABLE `dpto` (
+  `id` int(11) NOT NULL,
+  `Nome` varchar(45) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
 -- Estrutura da tabela `equipamento`
 --
 
@@ -37,15 +48,20 @@ CREATE TABLE `equipamento` (
   `patrimonial` varchar(11) NOT NULL,
   `dominio` varchar(4) DEFAULT NULL,
   `status` varchar(40) NOT NULL,
-  `descricao` varchar(100) NOT NULL
+  `iduser` int(11) DEFAULT NULL,
+  `descricao` varchar(100) NOT NULL,
+  `marca` varchar(45) DEFAULT NULL,
+  `modelo` varchar(100) DEFAULT NULL,
+  `servicetag` varchar(100) DEFAULT NULL,
+  `NF` int(100) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Extraindo dados da tabela `equipamento`
 --
 
-INSERT INTO `equipamento` (`id`, `nome_host`, `ip`, `processador`, `memoria`, `patrimonial`, `dominio`, `status`, `descricao`) VALUES
-(1, 'ti08', '192.168.0.154', 'Intel Core I3', '4GB', '0322', 's', 'ativo', 'Diego');
+INSERT INTO `equipamento` (`id`, `nome_host`, `ip`, `processador`, `memoria`, `patrimonial`, `dominio`, `status`, `iduser`, `descricao`, `marca`, `modelo`, `servicetag`, `NF`) VALUES
+(1, 'ti08', '192.168.0.154', 'Intel Core I3', '4GB', '0322', 's', 'ativo', NULL, 'Diego', NULL, NULL, NULL, NULL);
 
 -- --------------------------------------------------------
 
@@ -57,8 +73,8 @@ CREATE TABLE `software` (
   `id` int(11) NOT NULL,
   `nome` varchar(45) DEFAULT NULL,
   `chave` varchar(45) DEFAULT NULL,
-  `status` enum('ativo','inativo','disponivel') DEFAULT NULL,
-  `volume` enum('s','n') DEFAULT NULL,
+  `status` varchar(100) NOT NULL,
+  `volume` enum('s','n') NOT NULL,
   `quantidade` int(100) NOT NULL,
   `tiposoftware` int(1) NOT NULL,
   `descricao` text NOT NULL
@@ -69,18 +85,18 @@ CREATE TABLE `software` (
 --
 
 INSERT INTO `software` (`id`, `nome`, `chave`, `status`, `volume`, `quantidade`, `tiposoftware`, `descricao`) VALUES
-(1, NULL, NULL, NULL, NULL, 0, 1, 'Windows 7 pro x64'),
-(2, NULL, NULL, NULL, NULL, 0, 1, 'Windows 7 pro x86'),
-(3, NULL, NULL, NULL, NULL, 0, 1, 'Windows 8.0 pro x64'),
-(4, NULL, NULL, NULL, NULL, 0, 1, 'Windows 8.1 pro x64'),
-(5, NULL, NULL, NULL, NULL, 0, 1, 'Windows 10 pro x64'),
-(6, NULL, NULL, NULL, NULL, 0, 1, 'Windows 10 pro x86'),
-(7, NULL, NULL, NULL, NULL, 0, 1, 'Windows 8.0 pro x86'),
-(8, NULL, NULL, NULL, NULL, 0, 1, 'Windows 8.1 pro x86'),
-(9, NULL, NULL, NULL, NULL, 0, 2, 'Home and Businness 2013'),
-(10, NULL, NULL, NULL, NULL, 0, 2, 'Home and Businness 2010'),
-(11, NULL, NULL, NULL, NULL, 0, 2, 'Standard 2013'),
-(12, NULL, NULL, NULL, NULL, 0, 2, 'Office 365 Pro Plus');
+(1, NULL, NULL, '', '', 0, 1, 'Windows 7 pro x64'),
+(2, NULL, NULL, '', '', 0, 1, 'Windows 7 pro x86'),
+(3, NULL, NULL, '', '', 0, 1, 'Windows 8.0 pro x64'),
+(4, NULL, NULL, '', '', 0, 1, 'Windows 8.1 pro x64'),
+(5, NULL, NULL, '', '', 0, 1, 'Windows 10 pro x64'),
+(6, NULL, NULL, '', '', 0, 1, 'Windows 10 pro x86'),
+(7, NULL, NULL, '', '', 0, 1, 'Windows 8.0 pro x86'),
+(8, NULL, NULL, '', '', 0, 1, 'Windows 8.1 pro x86'),
+(9, NULL, NULL, '', '', 0, 2, 'Home and Businness 2013'),
+(10, NULL, NULL, '', '', 0, 2, 'Home and Businness 2010'),
+(11, NULL, NULL, '', '', 0, 2, 'Standard 2013'),
+(12, NULL, NULL, '', '', 0, 2, 'Office 365 Pro Plus');
 
 -- --------------------------------------------------------
 
@@ -91,8 +107,6 @@ INSERT INTO `software` (`id`, `nome`, `chave`, `status`, `volume`, `quantidade`,
 CREATE TABLE `tipo` (
   `id` int(11) NOT NULL,
   `nome` varchar(45) DEFAULT NULL,
-  `marca` varchar(45) DEFAULT NULL,
-  `modelo` varchar(45) DEFAULT NULL,
   `descricao` varchar(45) DEFAULT NULL,
   `status` varchar(45) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
@@ -107,19 +121,26 @@ CREATE TABLE `usuarios` (
   `id` int(11) NOT NULL,
   `nome` varchar(100) DEFAULT NULL,
   `senha` varchar(50) DEFAULT NULL,
-  `acesso` int(2) DEFAULT NULL
+  `acesso` int(2) DEFAULT NULL,
+  `departamento` varchar(45) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Extraindo dados da tabela `usuarios`
 --
 
-INSERT INTO `usuarios` (`id`, `nome`, `senha`, `acesso`) VALUES
-(1, 'Diego', '123', 1);
+INSERT INTO `usuarios` (`id`, `nome`, `senha`, `acesso`, `departamento`) VALUES
+(1, 'Diego', '123', 1, '');
 
 --
 -- Indexes for dumped tables
 --
+
+--
+-- Indexes for table `dpto`
+--
+ALTER TABLE `dpto`
+  ADD PRIMARY KEY (`id`);
 
 --
 -- Indexes for table `equipamento`
@@ -148,6 +169,12 @@ ALTER TABLE `usuarios`
 --
 -- AUTO_INCREMENT for dumped tables
 --
+
+--
+-- AUTO_INCREMENT for table `dpto`
+--
+ALTER TABLE `dpto`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT for table `equipamento`

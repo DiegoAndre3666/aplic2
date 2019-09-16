acbb7379d7e14e689721249b3b68a5d14afc5a8b
Diego Possidonio <diego.possidonio@frgranjeiro.local>
2019-09-16 13:34:05 -0300

    atualização tb usuarios

--- diego_inventario.sql
+++ diego_inventario.sql
@@ -1,39 +1,40 @@
 -- phpMyAdmin SQL Dump
--- version 4.0.10deb1ubuntu0.1
--- http://www.phpmyadmin.net
+-- version 4.8.5
+-- https://www.phpmyadmin.net/
 --
--- Servidor: localhost
--- Tempo de Geração: 30/08/2019 às 13:26
--- Versão do servidor: 5.5.62-0ubuntu0.14.04.1
--- Versão do PHP: 5.5.9-1ubuntu4.29
+-- Host: 127.0.0.1
+-- Generation Time: 16-Set-2019 às 18:31
+-- Versão do servidor: 10.1.40-MariaDB
+-- versão do PHP: 7.3.5
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
 SET time_zone = "+00:00";
 
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
+/*!40101 SET NAMES utf8mb4 */;
 
 --
--- Banco de dados: `diego_inventario`
+-- Database: `diego_inventario`
 --
 
 -- --------------------------------------------------------
 
 --
--- Estrutura para tabela `dpto`
+-- Estrutura da tabela `dpto`
 --
 
-CREATE TABLE IF NOT EXISTS `dpto` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `Nome` varchar(45) DEFAULT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=20 ;
+CREATE TABLE `dpto` (
+  `id` int(11) NOT NULL,
+  `Nome` varchar(45) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
--- Fazendo dump de dados para tabela `dpto`
+-- Extraindo dados da tabela `dpto`
 --
 
 INSERT INTO `dpto` (`id`, `Nome`) VALUES
@@ -60,11 +61,11 @@ INSERT INTO `dpto` (`id`, `Nome`) VALUES
 -- --------------------------------------------------------
 
 --
--- Estrutura para tabela `equipamento`
+-- Estrutura da tabela `equipamento`
 --
 
-CREATE TABLE IF NOT EXISTS `equipamento` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+CREATE TABLE `equipamento` (
+  `id` int(11) NOT NULL,
   `nome_host` varchar(45) DEFAULT NULL,
   `ip` varchar(30) DEFAULT NULL,
   `processador` varchar(50) DEFAULT NULL,
@@ -81,13 +82,11 @@ CREATE TABLE IF NOT EXISTS `equipamento` (
   `serial` int(100) NOT NULL,
   `idtipo` int(11) NOT NULL,
   `Portas` int(20) DEFAULT NULL,
-  `iddpto` int(11) NOT NULL,
-  PRIMARY KEY (`id`),
-  KEY `idtipo` (`idtipo`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=2 ;
+  `iddpto` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
--- Fazendo dump de dados para tabela `equipamento`
+-- Extraindo dados da tabela `equipamento`
 --
 
 INSERT INTO `equipamento` (`id`, `nome_host`, `ip`, `processador`, `memoria`, `patrimonial`, `dominio`, `status`, `iduser`, `descricao`, `marca`, `modelo`, `servicetag`, `NF`, `serial`, `idtipo`, `Portas`, `iddpto`) VALUES
@@ -96,18 +95,17 @@ INSERT INTO `equipamento` (`id`, `nome_host`, `ip`, `processador`, `memoria`, `p
 -- --------------------------------------------------------
 
 --
--- Estrutura para tabela `equipamentosoftware`
+-- Estrutura da tabela `equipamentosoftware`
 --
 
-CREATE TABLE IF NOT EXISTS `equipamentosoftware` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+CREATE TABLE `equipamentosoftware` (
+  `id` int(11) NOT NULL,
   `idequipamento` int(11) NOT NULL,
-  `idsoftware` int(11) NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=3 ;
+  `idsoftware` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
--- Fazendo dump de dados para tabela `equipamentosoftware`
+-- Extraindo dados da tabela `equipamentosoftware`
 --
 
 INSERT INTO `equipamentosoftware` (`id`, `idequipamento`, `idsoftware`) VALUES
@@ -117,23 +115,22 @@ INSERT INTO `equipamentosoftware` (`id`, `idequipamento`, `idsoftware`) VALUES
 -- --------------------------------------------------------
 
 --
--- Estrutura para tabela `software`
+-- Estrutura da tabela `software`
 --
 
-CREATE TABLE IF NOT EXISTS `software` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+CREATE TABLE `software` (
+  `id` int(11) NOT NULL,
   `nome` varchar(45) DEFAULT NULL,
   `chave` varchar(45) DEFAULT NULL,
   `status` varchar(100) NOT NULL,
   `volume` enum('s','n') NOT NULL,
   `quantidade` int(100) NOT NULL,
   `tiposoftware` int(1) NOT NULL,
-  `descricao` text NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=13 ;
+  `descricao` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
--- Fazendo dump de dados para tabela `software`
+-- Extraindo dados da tabela `software`
 --
 
 INSERT INTO `software` (`id`, `nome`, `chave`, `status`, `volume`, `quantidade`, `tiposoftware`, `descricao`) VALUES
@@ -153,17 +150,16 @@ INSERT INTO `software` (`id`, `nome`, `chave`, `status`, `volume`, `quantidade`,
 -- --------------------------------------------------------
 
 --
--- Estrutura para tabela `tipo`
+-- Estrutura da tabela `tipo`
 --
 
-CREATE TABLE IF NOT EXISTS `tipo` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `nome` varchar(45) NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=6 ;
+CREATE TABLE `tipo` (
+  `id` int(11) NOT NULL,
+  `nome` varchar(45) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
--- Fazendo dump de dados para tabela `tipo`
+-- Extraindo dados da tabela `tipo`
 --
 
 INSERT INTO `tipo` (`id`, `nome`) VALUES
@@ -176,34 +172,119 @@ INSERT INTO `tipo` (`id`, `nome`) VALUES
 -- --------------------------------------------------------
 
 --
--- Estrutura para tabela `usuarios`
+-- Estrutura da tabela `usuarios`
 --
 
-CREATE TABLE IF NOT EXISTS `usuarios` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+CREATE TABLE `usuarios` (
+  `id` int(11) NOT NULL,
   `nome` varchar(100) DEFAULT NULL,
+  `email` varchar(45) NOT NULL,
+  `ramal` int(5) NOT NULL,
   `senha` varchar(50) DEFAULT NULL,
   `acesso` int(2) DEFAULT NULL,
-  `iddpto` int(11) NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=2 ;
+  `iddpto` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
--- Fazendo dump de dados para tabela `usuarios`
+-- Extraindo dados da tabela `usuarios`
 --
 
-INSERT INTO `usuarios` (`id`, `nome`, `senha`, `acesso`, `iddpto`) VALUES
-(1, 'Diego', '123', 1, 1);
+INSERT INTO `usuarios` (`id`, `nome`, `email`, `ramal`, `senha`, `acesso`, `iddpto`) VALUES
+(1, 'Diego Andre', 'diego.possidonio@frangogranjeiro.com.br', 288, '0', 1, 1),
+(2, 'Danillo', 'danillo.sales@frangogranjeiro.com.br', 288, '0', 1, 1),
+(4, 'Pagani', 'thiago.pagani@frangogranjeiro.com.br', 287, '0', 1, 1);
 
 --
--- Restrições para dumps de tabelas
+-- Indexes for dumped tables
 --
 
 --
--- Restrições para tabelas `equipamento`
+-- Indexes for table `dpto`
+--
+ALTER TABLE `dpto`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `equipamento`
+--
+ALTER TABLE `equipamento`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `idtipo` (`idtipo`);
+
+--
+-- Indexes for table `equipamentosoftware`
+--
+ALTER TABLE `equipamentosoftware`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `software`
+--
+ALTER TABLE `software`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `tipo`
+--
+ALTER TABLE `tipo`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `usuarios`
+--
+ALTER TABLE `usuarios`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `dpto`
+--
+ALTER TABLE `dpto`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=20;
+
+--
+-- AUTO_INCREMENT for table `equipamento`
+--
+ALTER TABLE `equipamento`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `equipamentosoftware`
+--
+ALTER TABLE `equipamentosoftware`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT for table `software`
+--
+ALTER TABLE `software`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
+
+--
+-- AUTO_INCREMENT for table `tipo`
+--
+ALTER TABLE `tipo`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- AUTO_INCREMENT for table `usuarios`
+--
+ALTER TABLE `usuarios`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Limitadores para a tabela `equipamento`
 --
 ALTER TABLE `equipamento`
   ADD CONSTRAINT `equipamento_ibfk_1` FOREIGN KEY (`idtipo`) REFERENCES `equipamento` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
